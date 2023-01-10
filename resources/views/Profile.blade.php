76bbf6403ba053c5b17c3e7e9bf52a56afc5277e
MatsMardaga <104127794+MatsMardaga@users.noreply.github.com>
2023-01-10 17:34:39 +0100

    Initial commit

---
+++ resources/views/Profile.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile</title>
+    <link href="css/Bootstrap.css" rel="stylesheet"/>
+</head>
+<body>
+    <h1>This is the profile page</h1>
+    <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top">Back to top</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                        <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link" href="/">Home</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/News">News</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/Store">Store</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/FAQ">FAQ</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/Contact">contact</a></li>
+                        <li class="nav-item"><a class="nav-link" href="/Profile">Profile</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('Home');
+});
+Route::get('News', function () {
+    return view('News');
+});
+Route::get('Store', function () {
+    return view('Store');
+});
+Route::get('FAQ', function () {
+    return view('FAQ');
+});
+Route::get('Contact', function () {
+    return view('Contact');
+});
+Route::get('Profile', function () {
+    return view('Profile');
+});
