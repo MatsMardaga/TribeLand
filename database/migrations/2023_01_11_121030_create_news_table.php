1372915178e7b5cea9b9bc260a4cc482e9afdfaf
MatsMardaga <104127794+MatsMardaga@users.noreply.github.com>
2023-01-11 13:29:14 +0100

    new migrations

--- database/migrations/2023_01_11_121030_create_news_table.php
+++ database/migrations/2023_01_11_121030_create_news_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('news', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('message');
+            $table->integer('user_id');
             $table->timestamps();
         });
     }
