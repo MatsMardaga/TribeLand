95a6be53f03b2c245bb254475e96b6a65cc23ca2
MatsMardaga <104127794+MatsMardaga@users.noreply.github.com>
2023-01-13 16:39:10 +0100

    Contact page styling

--- resources/views/Contact/Contact.blade.php
+++ resources/views/Contact/Contact.blade.php
@@ -1,21 +1,88 @@
 @extends( (Auth::check() && Auth::user()->is_admin) ? 'layouts.admin' : 'layouts.app')
 @section('content')
 
-<header>
-    <div class="container px-4 text-center">
-        <h1 class="fw-bolder">Welcome to TribeLand</h1>
-        <hr>
-        <p class="lead">
-            Tribeland is a webstore that focusses on Norse and Celtic mythology but as the name implies, we sell tribal stuff in general.
-            <br>
-            <br>
-            What distinguishes us from other stores is that our focus is not only on making money and selling trendy tribal and mythological merchandise,
-            we also aim to educate people on those topics by providing you with information about the items you pruchase.
-            Our admins will often post exciting news about archeological finds, historical discoveries and blog posts about living in nature.
-            <br>
-            <br>
-            Join our community so you can interact and suggest topic we can write about.
-        </p>
+<!--Section: Contact v.2-->
+<section class="mb-4 container">
+
+    <!--Section heading-->
+    <h2 class="h1-responsive font-weight-bold text-center my-4">Contact us</h2>
+    <!--Section description-->
+    <p class="text-center w-responsive mx-auto mb-5">Do you have any questions? Please do not hesitate to contact us directly. Our team will come back to you within
+        a matter of hours to help you.</p>
+
+    <div class="row">
+
+        <!--Grid column-->
+        <div class="col-md-9 mb-md-0 mb-5 justify-content-center container">
+            <form id="contact-form" name="contact-form" action="mail.php" method="POST" class="">
+                @csrf
+
+                <!--Grid row-->
+                <div class="row">
+
+                    <!--Grid column-->
+                    <div class="col-md-6">
+                        <div class="md-form mb-0">
+                            <input type="text" id="name" name="name" class="form-control" value="{{ Auth::user()->name }}">
+                            <label for="name" class="">Name</label>
+                        </div>
+                    </div>
+                    <!--Grid column-->
+
+                    <!--Grid column-->
+                    <div class="col-md-6">
+                        <div class="md-form mb-0">
+                            <input type="text" id="email" name="email" class="form-control" value="{{ Auth::user()->email }}">
+                            <label for="email" class="">Email</label>
+                        </div>
+                    </div>
+                    <!--Grid column-->
+
+                </div>
+                <!--Grid row-->
+
+                <!--Grid row-->
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="md-form mb-0">
+                            <input type="text" id="subject" name="subject" class="form-control">
+                            <label for="subject" class="">Subject</label>
+                        </div>
+                    </div>
+                </div>
+                <!--Grid row-->
+
+                <!--Grid row-->
+                <div class="row">
+
+                    <!--Grid column-->
+                    <div class="col-md-12">
+
+                        <div class="md-form">
+                            <textarea type="text" id="message" name="message" rows="10" class="form-control md-textarea"></textarea>
+                            <label for="message">Message</label>
+                        </div>
+
+                    </div>
+                </div>
+                <!--Grid row-->
+
+            </form>
+
+            <div class="text-center text-md-left ">
+                <a class="btn btn-primary" onclick="document.getElementById('contact-form').submit();">Send</a>
+            </div>
+            <div class="status"></div>
+        </div>
     </div>
-</header>
+    <br>
+    <div class="col-md-3 container justify-content-center">
+        <p class="text-center" >TribeLand Belgium</p>
+        <p class="text-center">+32 471 783 306</p>
+        <p class="text-center">mats.mardaga@gmail.com</p>
+
+
+    </div>
+
+</section>
 @endsection
