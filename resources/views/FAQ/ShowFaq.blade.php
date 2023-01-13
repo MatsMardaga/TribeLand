daec4e9551fbe2d127f1fcb537a3bc425646b90b
MatsMardaga <104127794+MatsMardaga@users.noreply.github.com>
2023-01-14 00:33:33 +0100

    edit and delete buttons and links

--- resources/views/FAQ/ShowFaq.blade.php
+++ resources/views/FAQ/ShowFaq.blade.php
@@ -7,12 +7,31 @@
     <div class="row gx-4 justify-content-center">
         <div class="col-lg-8">
             @foreach($category as $item)
+
             <h1>{{ $item->name }}</h1>
+            @if(Auth::check() && Auth::user()->is_admin)
+            <div class="">
+            <a href="/">Edit category</a>
+            <form method="POST" action="{{route('News.destroy', $item->id)}}">
+                @csrf
+                @method("delete")
+                <input type="submit" value="Delete category" class="btn" onclick="return confirm('Delete category?');">
+            </form>
+            </div>
+            @endif
             @foreach($item->FaQCategoryQuestion as $question)
             <p>
                 <a class="btn btn-primary" data-bs-toggle="collapse" href="#collapse{{$item->id}}{{$question->id}}" role="button" aria-expanded="false" aria-controls="collapseExample">
                     {{ $question->name }}
                 </a>
+                @if(Auth::check() && Auth::user()->is_admin)
+                <a href="/">Edit Question and content</a>
+            <form method="POST" action="{{route('News.destroy', $item->id)}}">
+                @csrf
+                @method("delete")
+                <input type="submit" value="Delete question and content" class="btn" onclick="return confirm('Delete question and content?');">
+            </form>
+            @endif
             </p>
             <div class="collapse" id="collapse{{$item->id}}{{$question->id}}">
                 <div class="card card-body">
@@ -26,6 +45,5 @@
             @endforeach
         </div>
     </div>
-
 </section>
 @endsection
