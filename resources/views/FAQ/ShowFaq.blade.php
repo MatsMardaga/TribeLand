2d3f8fe3e9e140c7e45bac80d9fe79cc2c3ffe03
MatsMardaga <104127794+MatsMardaga@users.noreply.github.com>
2023-01-13 23:20:17 +0100

    Every category collapses

--- resources/views/FAQ/ShowFaq.blade.php
+++ resources/views/FAQ/ShowFaq.blade.php
@@ -1,21 +1,26 @@
 @extends( (Auth::check() && Auth::user()->is_admin) ? 'layouts.admin' : 'layouts.app')
 
 @section('content')
-
-<header>
-    <section class="bg-black" id="about">
-        <div class="container px-4">
-            <div class="row gx-4 justify-content-center">
-                <div class="col-lg-8 text-white">
-                    <h2>FAQ page</h2>
-                    @foreach($category as $item)
-                    <h3>{{ $item->name}}</h3>
-                    <hr>
-                    @endforeach
+<section class="bg-white" id="about">
+<h2 class="row gx-4 justify-content-center">FAQ page</h2>
+    <div class="row gx-4 justify-content-center">
+        <div class="col-lg-8">
+            @foreach($category as $item)
+            <p>
+                <a class="btn btn-primary" data-bs-toggle="collapse" href="#collapseExample{{$loop->iteration}}" role="button" aria-expanded="false" aria-controls="collapseExample">
+                    {{ $item->name}}
+                </a>
+            </p>
+            <div class="collapse" id="collapseExample{{$loop->iteration}}">
+                <div class="card card-body">
+                    <p>Some placeholder content for the collapse component. This panel is hidden by default but revealed when the user activates the relevant trigger.</p>
                 </div>
             </div>
+            <h3 data-bs-toggle="collapse"></h3>
+            <br>
+            @endforeach
         </div>
-    </section>
-    <header>
+    </div>
 
-        @endsection
+</section>
+@endsection
